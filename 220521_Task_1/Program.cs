ecf1ebb3a2349193afa813e11109954c047ab324
vmaevski <vmaevski@mail.ru>
2022-05-22 07:51:17 +0700

    Task 1 completed.

--- 220521_Task_1/Program.cs
+++ 220521_Task_1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// 1. Напишите цикл, который принимает на вход два числа (A и B) и 
 // возводит число A в натуральную степень B.
 
-int Exponentiation(int a, int b)
+double Exponentiation(double a, int b)
 {
-    int result = 1;
+    double result = 1;
     for (int i = 1; i <= b; i++)
     {
         result *= a;
@@ -11,6 +11,6 @@ int Exponentiation(int a, int b)
     return result;
 }
 
-Console.WriteLine(Exponentiation(-2, 3));
+Console.WriteLine(Exponentiation(-2.5, 3));
 Console.WriteLine(Exponentiation(3, 3));
 Console.WriteLine(Exponentiation(10, 6));

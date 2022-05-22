988f4f07910bbc2a981ac94376f16edbbffff28e
vmaevski <vmaevski@mail.ru>
2022-05-22 08:08:29 +0700

    Task 2 completed.

---
+++ 220521_Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+int SumNum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+Console.WriteLine(SumNum(12345));
